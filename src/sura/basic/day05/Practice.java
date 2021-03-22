cb1ed9e098ab9cc33093410f5e440c21a48e60ba
suralee <suraleeeee@gmail.com>
2021-03-22 17:24:01 +0900

    조건문, 반복문, 배열

---
+++ src/sura/basic/day05/Practice.java
@@ -0,0 +1,13 @@
+package sura.basic.day05;
+
+public class Practice {
+	public static void main(String args) {
+		// 조건문, 반복문, 배열
+
+		// Q54
+		char[] letter = { 'a', 'b', 'c' };
+		for (int index = 0; index < letter.length; index++)
+			System.out.println(letter[index] + ", ");
+
+	}
+}
